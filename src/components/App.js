91057a455edeeaed2500d58eeb462f28b8725b3e
Martin <martingakuya@gmail.com>
2023-09-10 04:49:42 +0300

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,52 @@
 import React, { useState } from "react";
-import ShoppingList from "./ShoppingList";
-import Header from "./Header";
-import itemData from "../data/items";
+import Filter from "./Filter";
+import ItemForm from "./ItemForm";
 
 function App() {
-  const [items, setItems] = useState(itemData);
-  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [items, setItems] = useState([]);
+  const [search, setSearch] = useState("");
+  const [category, setCategory] = useState("All");
 
-  function handleDarkModeClick() {
-    setIsDarkMode((isDarkMode) => !isDarkMode);
-  }
+  // Function to handle category change
+  const handleCategoryChange = (event) => {
+    setCategory(event.target.value);
+  };
+
+  // Function to handle search input change
+  const handleSearchChange = (event) => {
+    setSearch(event.target.value);
+  };
+
+  // Function to add a new item to the list
+  const handleItemFormSubmit = (newItem) => {
+    setItems([...items, newItem]);
+  };
+
+  // Filter items based on search and category
+  const filteredItems = items.filter((item) => {
+    if (category === "All" || item.category === category) {
+      return item.name.toLowerCase().includes(search.toLowerCase());
+    }
+    return false;
+  });
 
   return (
-    <div className={"App " + (isDarkMode ? "dark" : "light")}>
-      <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
-      <ShoppingList items={items} />
+    <div className="App">
+      <h1>Shopping List</h1>
+      <Filter
+        search={search}
+        category={category}
+        onCategoryChange={handleCategoryChange}
+        onSearchChange={handleSearchChange}
+      />
+      <ItemForm onItemFormSubmit={handleItemFormSubmit} />
+      <ul>
+        {filteredItems.map((item) => (
+          <li key={item.id}>
+            {item.name} - {item.category}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,17 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { v4 as uuid } from "uuid";
 
-function ItemForm(props) {
+function ItemForm({ onItemFormSubmit }) {
+  const [itemName, setItemName] = useState("");
+  const [itemCategory, setItemCategory] = useState("Produce");
+
+  // Function to handle form submission
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // Create a new item object
+    const newItem = {
+      id: uuid(),
+      name: itemName,
+      category: itemCategory,
+    };
+
+    // Call the callback function to add the new item
+    onItemFormSubmit(newItem);
+
+    // Clear form inputs
+    setItemName("");
+    setItemCategory("Produce");
+  };
+
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={handleSubmit}>
       <label>
         Name:
-        <input type="text" name="name" />
+        <input
+          type="text"
+          name="name"
+          value={itemName}
+          onChange={(e) => setItemName(e.target.value)}
+        />
       </label>
 
       <label>
         Category:
-        <select name="category">
+        <select
+          name="category"
+          value={itemCategory}
+          onChange={(e) => setItemCategory(e.target.value)}
+        >
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
